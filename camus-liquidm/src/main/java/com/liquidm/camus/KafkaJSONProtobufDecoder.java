067cf831df5076faa2e1908620d972871bbd75cb
Hagen Rother <hagen.rother@liquidm.com>
2014-04-11 15:57:23 +0200

    try w/o protostuff

--- camus-liquidm/src/main/java/com/liquidm/camus/KafkaJSONProtobufDecoder.java
+++ camus-liquidm/src/main/java/com/liquidm/camus/KafkaJSONProtobufDecoder.java
@@ -1,11 +1,10 @@
 package com.liquidm.camus;
 
-import com.dyuproject.protostuff.JsonIOUtil;
-import com.dyuproject.protostuff.runtime.RuntimeSchema;
 import com.liquidm.Events;
 import com.linkedin.camus.coders.CamusWrapper;
 import com.linkedin.camus.coders.MessageDecoder;
 import com.linkedin.camus.coders.MessageDecoderException;
+import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
 
 public class KafkaJSONProtobufDecoder extends MessageDecoder<byte[], Events.EventLogged> {
@@ -14,8 +13,10 @@ public class KafkaJSONProtobufDecoder extends MessageDecoder<byte[], Events.Even
   @Override
   public CamusWrapper<Events.EventLogged> decode(byte[] message) {
     try {
+      final JSONRecord record = new JSONRecord(mapper.readValue(message, JsonNode.class));
       Events.EventLogged.Builder event = Events.EventLogged.newBuilder();
-      JsonIOUtil.mergeFrom(message, event, RuntimeSchema.getSchema(Events.EventLogged.Builder.class), false);
+      event.setTimestamp((Float) record.get("timestamp"));
+      event.setType(Events.EventLogged.EventType.valueOf((Integer) record.get("type")));
 
       return new CamusWrapper<Events.EventLogged>(event.build(), ((long) event.getTimestamp()) * 1000);
     } catch (Exception e) {
